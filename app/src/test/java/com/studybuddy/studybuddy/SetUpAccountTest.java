46b850b1e91800d8600dfc09772f766115675971
Unknown <kylewbassignani@ufl.edu>
2018-03-16 20:07:03 -0400

    create account with email

---
+++ app/src/test/java/com/studybuddy/studybuddy/SetUpAccountTest.java
@@ -0,0 +1,26 @@
+package com.studybuddy.studybuddy;
+
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by kyle on 3/14/18.
+ */
+public class SetUpAccountTest {
+    @Test
+    public void onCreate() throws Exception {
+    }
+
+    @Test
+    public void onBackPressed() throws Exception {
+    }
+
+    @Test
+    public void delete() throws Exception {
+    }
+
+}
