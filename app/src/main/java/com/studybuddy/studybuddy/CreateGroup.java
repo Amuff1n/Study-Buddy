f6ef07718051d4571e5530affc9a9703e7d945eb
Thoreaufare <kolby.rottero@gmail.com>
2018-03-22 18:26:51 -0400

    * added a drop down list for selecting a location. Current options are Marston, Library West, Reitz Union, and Newell Hall

--- app/src/main/java/com/studybuddy/studybuddy/CreateGroup.java
+++ app/src/main/java/com/studybuddy/studybuddy/CreateGroup.java
@@ -6,8 +6,11 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -17,14 +20,15 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.lang.reflect.Array;
 import java.util.HashMap;
 import java.util.Map;
 
-public class CreateGroup extends AppCompatActivity {
+public class CreateGroup extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private FirebaseAuth mAuth;
     private FirebaseFirestore mFirestore;
     private EditText mClass;
-    private EditText mLocation;
+    private Spinner mLocation;
     private EditText mDescription;
     private Button mCreateGroup;
 
@@ -37,21 +41,35 @@ public class CreateGroup extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         mClass = findViewById(R.id.group_class_field);
         mLocation = findViewById(R.id.group_location_field);
+
         mDescription = findViewById(R.id.group_desc_field);
         mCreateGroup = findViewById(R.id.create_group_button);
-
+        //Have to make an adapter to fill in drop down menu items with string array
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.locations_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mLocation.setAdapter(adapter);
+        mLocation.setOnItemSelectedListener(this);
         mFirestore = FirebaseFirestore.getInstance();
 
         mCreateGroup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String groupClass = mClass.getText().toString();
-                String groupLocation = mLocation.getText().toString();
+                String groupLocation = mLocation.getSelectedItem().toString();
                 String groupDesc = mDescription.getText().toString();
                 createGroup(groupClass, groupLocation, groupDesc);
                 finish();
             }
         });
+    }
+
+    //Methods for getting selected drop down list item
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        parent.getItemAtPosition(pos);
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 
     public boolean createGroup(String groupClass, String groupLocation, String groupDesc) {
