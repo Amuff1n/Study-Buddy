aede7f0cd7432cebba9981415802714533ba42d7
greenphantom <daniel.tymecki@gmail.com>
2018-04-09 22:05:57 -0400

    Added New Functionality for Basic Searches
    Searches still need to be defined, but the basic layout is now in place.

--- app/src/main/java/com/studybuddy/studybuddy/FindGroups.java
+++ app/src/main/java/com/studybuddy/studybuddy/FindGroups.java
@@ -2,12 +2,59 @@ package com.studybuddy.studybuddy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class FindGroups extends AppCompatActivity {
+
+    ListView search_groups;
+    ArrayAdapter <String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_groups);
+
+        search_groups = (ListView) findViewById(R.id.search_groups);
+
+        ArrayList<String> groups = new ArrayList<>();
+        groups.addAll(Arrays.asList(getResources().getStringArray(R.array.mygroups)));
+
+        adapter = new ArrayAdapter<String>(
+                FindGroups.this,
+                android.R.layout.simple_list_item_1,
+                groups
+        );
+
+        search_groups.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_menu,menu);
+        MenuItem item = menu.findItem(R.id.search_groups);
+        SearchView searchView = (SearchView) item.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
+            @Override
+            public boolean onQueryTextSubmit(String s){
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s){
+                adapter.getFilter().filter(s);
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
